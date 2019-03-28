bb9780e96e41b6ebd898d77b6b84c490249a06dd
SilverMaRu <setsuna00@qq.com>
2019-03-29 00:22:20 +0800

    添加作业20190328
    1.调整目录结构
    2.添加unity实操作业

---
+++ Unity Homework/Assets/Scripts/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+    private ScoreModel scoreModel;
+    private string saveFilePath;
+
+	// Use this for initialization
+	void Start () {
+        saveFilePath = Application.persistentDataPath + "/save.sav";
+        Load();
+        DisplayScore();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddScore(int score)
+    {
+        if (score <= 0)
+        {
+            return;
+        }
+
+        scoreModel.score += score;
+        scoreModel.level = scoreModel.score / 100 + 1;
+        Save();
+        DisplayScore();
+    }
+
+    private void DisplayScore()
+    {
+        Text scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        scoreText.text = "Score =" + scoreModel.score.ToString();
+
+        Text levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Level =" + scoreModel.level.ToString();
+    }
+
+    private void Load()
+    {
+        scoreModel = (ScoreModel)FileTool.ReadSaveFile(saveFilePath);
+        if(scoreModel == null)
+        {
+            scoreModel = new ScoreModel();
+        }
+    }
+
+    private void Save()
+    {
+        FileTool.WriteSaveFile(saveFilePath, scoreModel);
+    }
+}
---
+++ Unity Homework/Assets/Scripts/FileTool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public class FileTool{
+    public static object ReadSaveFile(string path)
+    {
+        object ret = null;
+        if (File.Exists(path))
+        {
+            FileStream fs = File.Open(path, FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            ret = bf.Deserialize(fs);
+            fs.Close();
+        }
+        return ret;
+    }
+
+    public static void WriteSaveFile(string path, object data)
+    {
+        FileStream fs = File.OpenWrite(path);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(fs, data);
+        fs.Close();
+    }
+}

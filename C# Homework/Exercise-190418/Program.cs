df40aaee4b5aabf087e98fe2e25e76573b1d05f6
SilverMaRu <setsuna00@qq.com>
2019-04-18 16:09:17 +0800

    添加作业20190418

---
+++ C# Homework/Exercise-190418/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_190418
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarEngine carEngine = new CarEngine();
+            SuperEngine superEngine = new SuperEngine();
+
+            CarEngine engine = carEngine;
+            engine.TurnOn();
+            engine.TurnOff();
+
+            engine = superEngine;
+            engine.TurnOn();
+            engine.TurnOff();
+        }
+    }
+}
